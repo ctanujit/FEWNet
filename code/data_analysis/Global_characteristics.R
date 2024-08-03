c1ad269afbe1cced8b348591031376885de41d12
SHOVON SENGUPTA <ssg.plabon@gmail.com>
2024-08-03 15:32:14 -0400

    Update Global_characteristics.R

--- code/data_analysis/Global_characteristics.R
+++ code/data_analysis/Global_characteristics.R
@@ -1,6 +1,6 @@
 ############################ Global Characteristics: CPI Inflation, log(EPU) and GPRC: BRIC ######################
 
-# install.packages('fpp2')
+# install.packages('entropy')
 
 # Important Libraries
 library(nonlinearTseries)
@@ -24,14 +24,35 @@ library(GeneCycle)
 library(fpp2)
 library(seastests)
 library(entropy)
+library(zoo)
+library(sandwich)
+library(strucchange)
+library(tseries)
 
 ################## Brazil ############################
-setwd("/dataset/brazil")
+setwd("/data/brazil")
 getwd()
 
 # Data
 cpi.df <- read.csv("Brazil_CPI_inf_rate_Monthly_base_mulvar_cpi_epu_gprc_202201.csv",header=TRUE)
 str(cpi.df)
+
+# Calculate Coefficient of Variation and STDV
+#calculate CV for each column in data frame
+sapply(cpi.df[-1], function(x) sd(x) / mean(x) * 100)
+#calculate STDV for each column in data frame
+sapply(cpi.df[-1], function(x) sd(x))
+
+# entropy(cpi.df$cpi_inflation_rate, method="MM")
+sapply(cpi.df[-1], function(x) entropy(x, method="MM"))
+
+# Find the Min-value, Max Value, Mean Value, Median Value, Q1-value, Q3-value
+sapply(cpi.df[-1], function(x) min(x))
+sapply(cpi.df[-1], function(x) max(x))
+sapply(cpi.df[-1], function(x) mean(x))
+sapply(cpi.df[-1], function(x) median(x))
+sapply(cpi.df[-1], function(x) quantile(x, prob=c(.25,.5,.75), type=1))
+
 
 # Check for Skewness
 skewness(cpi.df$CPI_inflation_rate)
@@ -64,6 +85,76 @@ kpss.test(cpi.df$CPI_inflation_rate) #Non-stationary
 kpss.test(cpi.df$log_epu) #Non-stationary
 kpss.test(cpi.df$gprc_bra) #Stationary
 
+########################## Statistical Test for Outlier Detection ########################
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+CPI_brazil <- ts(cpi.df$CPI_inflation_rate, start = 2003, end = 2021, frequency = 12)
+CPI_brazil
+length(CPI_brazil)
+
+log_epu_brazil <- ts(cpi.df$log_epu, start = 2003, end = 2021, frequency = 12)
+log_epu_brazil
+length(log_epu_brazil)
+
+gprc_bra_brazil <- ts(cpi.df$gprc_bra, start = 2003, end = 2021, frequency = 12)
+gprc_bra_brazil
+length(gprc_bra_brazil)
+
+time <- c(1:217)
+
+library(car)
+# CPI
+test_out_cpi <- outlierTest(lm(CPI_brazil~time, 
+                               cutoff=0.05, 
+                               n.max=10, 
+                               order=TRUE, 
+                               labels=names(rstudent)))
+print(test_out_cpi)
+
+# log(EPU)
+test_out_epu <- outlierTest(lm(log_epu_brazil~time, 
+                               cutoff=0.05, 
+                               n.max=10, 
+                               order=TRUE, 
+                               labels=names(rstudent)))
+print(test_out_epu)
+
+# GPRC
+test_out_gprc <- outlierTest(lm(gprc_bra_brazil~time, 
+                                cutoff=0.05, 
+                                n.max=10, 
+                                order=TRUE, 
+                                labels=names(rstudent)))
+print(test_out_gprc)
+
+############## Test for structural break-points: OLS - CUSUM ##############
+# Identify the breakpoints
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_brazil <- ts(cpi.df$CPI_inflation_rate, start = 2003, end = 2021, frequency = 6)
+CPI_brazil
+length(CPI_brazil)
+time <- c(1:109)
+
+# Plot the Series
+plot(CPI_brazil, type = "l", las = 1, xaxs = "i", xlab = "", ylab = "", col = "blue")
+
+# Identify the breakpoints
+length(CPI_brazil)
+CPI_brazil_bp <- breakpoints(CPI_brazil ~ time, h = 7)
+CPI_brazil_bp
+
+# Check for the statistical significance of the break-points
+coef(CPI_brazil_bp)
+
+# Check for the statistical significance of the break-points
+addi_CPI_BP <- efp(CPI_brazil ~ breakfactor(CPI_brazil_bp), type = "OLS-CUSUM")
+plot(addi_CPI_BP)
+
 ################## Russia ############################
 setwd("/dataset/russia")
 getwd()
@@ -71,6 +162,18 @@ getwd()
 # Data
 cpi.df <- read.csv("RUS_CPI_inf_rate_Monthly_mulvar_epu_gprc_202201.csv",header=TRUE)
 str(cpi.df)
+
+#calculate CV for each column in data frame
+sapply(cpi.df[-1], function(x) sd(x) / mean(x) * 100)
+# entropy(cpi.df$cpi_inflation_rate, method="MM")
+sapply(cpi.df[-1], function(x) entropy(x, method="MM"))
+
+# Find the Min-value, Max Value, Mean Value, Median Value, Q1-value, Q3-value
+sapply(cpi.df[-1], function(x) min(x))
+sapply(cpi.df[-1], function(x) max(x))
+sapply(cpi.df[-1], function(x) mean(x))
+sapply(cpi.df[-1], function(x) median(x))
+sapply(cpi.df[-1], function(x) quantile(x, prob=c(.25,.5,.75), type=1))
 
 # Check for Skewness
 skewness(cpi.df$cpi_inflation_rate)
@@ -103,6 +206,75 @@ kpss.test(cpi.df$cpi_inflation_rate) #Non-stationary
 kpss.test(cpi.df$log_epu) #Non-stationary
 kpss.test(cpi.df$gprc_rus) #Non-stationary
 
+########################## Statistical Test for Outlier Detection ########################
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+
+CPI_russia <- ts(cpi.df$cpi_inflation_rate, start = 2003, end = 2021, frequency = 12)
+CPI_russia
+length(CPI_russia)
+time <- c(1:217)
+
+log_epu_russia <- ts(cpi.df$log_epu, start = 2003, end = 2021, frequency = 12)
+log_epu_russia
+length(log_epu_russia)
+time <- c(1:217)
+
+gprc_russia <- ts(cpi.df$gprc_rus, start = 2003, end = 2021, frequency = 12)
+gprc_russia
+length(gprc_russia)
+time <- c(1:217)
+
+library(car)
+test_out_cpi <- outlierTest(lm(CPI_russia~time, 
+                               cutoff=0.05, 
+                               n.max=10, 
+                               order=TRUE, 
+                               labels=names(rstudent)))
+print(test_out_cpi)
+
+test_out_log_epu <- outlierTest(lm(log_epu_russia~time, 
+                                   cutoff=0.05, 
+                                   n.max=10, 
+                                   order=TRUE, 
+                                   labels=names(rstudent)))
+print(test_out_log_epu)
+
+
+test_out_gprc <- outlierTest(lm(gprc_russia~time, 
+                                cutoff=0.05, 
+                                n.max=10, 
+                                order=TRUE, 
+                                labels=names(rstudent)))
+print(test_out_gprc)
+
+############## Test for structural break-points: OLS - CUSUM ##############
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_russia <- ts(cpi.df$cpi_inflation_rate, start = 2003, end = 2021, frequency = 6)
+CPI_russia
+length(CPI_russia)
+time <- c(1:109)
+
+# Plot the Series
+plot(CPI_russia, type = "l", las = 1, xaxs = "i", xlab = "", ylab = "", col = "blue")
+
+# Identify the breakpoints
+length(CPI_russia)
+CPI_russia_bp <- breakpoints(CPI_russia ~ time, h=7)
+CPI_russia_bp
+
+# Check for the statistical significance of the break-points
+coef(CPI_russia_bp)
+
+# Check for the statistical significance of the break-points
+addi_CPI_BP <- efp(CPI_russia ~ breakfactor(CPI_russia_bp), type = "OLS-CUSUM")
+plot(addi_CPI_BP)
+
 ################## India ############################
 setwd("/dataset/india")
 getwd()
@@ -110,6 +282,18 @@ getwd()
 # Data
 cpi.df <- read.csv("India_CPI_inf_rate_Monthly_base_mulvar_cpi_epu_gprc_202201.csv",header=TRUE)
 str(cpi.df)
+
+#calculate CV for each column in data frame
+sapply(cpi.df[-1], function(x) sd(x) / mean(x) * 100)
+# entropy(cpi.df$cpi_inflation_rate, method="MM")
+sapply(cpi.df[-1], function(x) entropy(x, method="MM"))
+
+# Find the Min-value, Max Value, Mean Value, Median Value, Q1-value, Q3-value
+sapply(cpi.df[-1], function(x) min(x))
+sapply(cpi.df[-1], function(x) max(x))
+sapply(cpi.df[-1], function(x) mean(x))
+sapply(cpi.df[-1], function(x) median(x))
+sapply(cpi.df[-1], function(x) quantile(x, prob=c(.25,.5,.75), type=1))
 
 # Check for Skewness
 skewness(cpi.df$CPI_inflation_Rate)
@@ -142,9 +326,92 @@ kpss.test(cpi.df$CPI_inflation_Rate) #Non-stationary
 kpss.test(cpi.df$log_epu) #Non-stationary
 kpss.test(cpi.df$gprc_ind) #Non-stationary
 
+########################## Statistical Test for Outlier Detection ########################
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$Date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_india <- ts(cpi.df$CPI_inflation_Rate, start = 2003, end = 2021, frequency = 12)
+CPI_india
+length(CPI_india)
+time <- c(1:217)
+
+# Convert CPI inflation into a time series object
+log_epu_india <- ts(cpi.df$log_epu, start = 2003, end = 2021, frequency = 12)
+log_epu_india
+length(log_epu_india)
+time <- c(1:217)
+
+# Convert CPI inflation into a time series object
+gprc_india <- ts(cpi.df$gprc_ind, start = 2003, end = 2021, frequency = 12)
+gprc_india
+length(gprc_india)
+time <- c(1:217)
+
+# Outlier Test using Bonferroni Outlier Test
+test_out_cpi <- outlierTest(lm(CPI_india~time, 
+                               cutoff=0.05, 
+                               n.max=10, 
+                               order=TRUE, 
+                               labels=names(rstudent)))
+print(test_out_cpi)
+
+test_out_log_epu <- outlierTest(lm(log_epu_india~time, 
+                                   cutoff=0.05, 
+                                   n.max=10, 
+                                   order=TRUE, 
+                                   labels=names(rstudent)))
+print(test_out_log_epu)
+
+test_out_gprc <- outlierTest(lm(gprc_india~time, 
+                                cutoff=0.05, 
+                                n.max=10, 
+                                order=TRUE, 
+                                labels=names(rstudent)))
+print(test_out_gprc)
+
+############## Test for structural break-points: OLS - CUSUM ##############
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$Date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_india <- ts(cpi.df$CPI_inflation_Rate, start = 2003, end = 2021, frequency = 6)
+CPI_india
+length(CPI_india)
+time <- c(1:109)
+
+# Plot the Series
+plot(CPI_india, type = "l", las = 1, xaxs = "i", xlab = "", ylab = "", col = "blue")
+
+# Identify the breakpoints
+length(CPI_india)
+CPI_india_bp <- breakpoints(CPI_india ~ time, h = 7)
+CPI_india_bp
+
+# Check for the statistical significance of the break-points
+coef(CPI_india_bp)
+
+# Check for the statistical significance of the break-points
+addi_CPI_BP <- efp(CPI_india ~ breakfactor(CPI_india_bp), type = "OLS-CUSUM")
+plot(addi_CPI_BP)
+
 ################## China ############################
 setwd("/dataset/india")
 getwd()
+
+#calculate CV for each column in data frame
+sapply(cpi.df[-1], function(x) sd(x) / mean(x) * 100)
+# entropy(cpi.df$cpi_inflation_rate, method="MM")
+sapply(cpi.df[-1], function(x) entropy(x, method="MM"))
+
+# Find the Min-value, Max Value, Mean Value, Median Value, Q1-value, Q3-value
+sapply(cpi.df[-1], function(x) min(x))
+sapply(cpi.df[-1], function(x) max(x))
+sapply(cpi.df[-1], function(x) mean(x))
+sapply(cpi.df[-1], function(x) median(x))
+sapply(cpi.df[-1], function(x) quantile(x, prob=c(.25,.5,.75), type=1))
 
 # Data
 cpi.df <- read.csv("China_CPI_inf_rate_Monthly_mulvariate_epu_gprc_202201.csv",header=TRUE)
@@ -180,5 +447,74 @@ hurstexp(cpi.df$gprc_chn)
 kpss.test(cpi.df$cpi_inflation_rate) #Stationary
 kpss.test(cpi.df$log_scmp_epu) #Non-stationary
 kpss.test(cpi.df$gprc_chn) #Non-stationary
+
+########################## Statistical Test for Outlier Detection ########################
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_china <- ts(cpi.df$cpi_inflation_rate, start = 2003, end = 2021, frequency = 12)
+CPI_china
+length(CPI_china)
+time <- c(1:217)
+
+log_scmp_epu_china <- ts(cpi.df$log_scmp_epu, start = 2003, end = 2021, frequency = 12)
+log_scmp_epu_china
+length(log_scmp_epu_china)
+time <- c(1:217)
+
+gprc_china <- ts(cpi.df$gprc_chn, start = 2003, end = 2021, frequency = 12)
+gprc_china
+length(gprc_china)
+time <- c(1:217)
+
+# Outlier Test using Bonferroni Outlier Test
+library(car)
+test_out_cpi <- outlierTest(lm(CPI_china~time, 
+                               cutoff=0.05, 
+                               n.max=10, 
+                               order=TRUE, 
+                               labels=names(rstudent)))
+print(test_out_cpi)
+
+test_out_log_epu <- outlierTest(lm(log_scmp_epu_china~time, 
+                                   cutoff=0.05, 
+                                   n.max=10, 
+                                   order=TRUE, 
+                                   labels=names(rstudent)))
+print(test_out_log_epu)
+
+test_out_gprc <- outlierTest(lm(gprc_china~time, 
+                                cutoff=0.05, 
+                                n.max=10, 
+                                order=TRUE, 
+                                labels=names(rstudent)))
+print(test_out_gprc)
+############## Test for structural break-points: OLS - CUSUM ##############
+# Convert Date into a numeric quantity
+cpi.df$date_n <- as.numeric(cpi.df$date)
+str(cpi.df)
+
+# Convert CPI inflation into a time series object
+CPI_china <- ts(cpi.df$cpi_inflation_rate, start = 2003, end = 2021, frequency = 6)
+CPI_china
+length(CPI_china)
+time <- c(1:109)
+
+# Plot the Series
+plot(CPI_china, type = "l", las = 1, xaxs = "i", xlab = "", ylab = "", col = "blue")
+
+# Identify the breakpoints
+length(CPI_china)
+CPI_china_bp <- breakpoints(CPI_china ~ time, h = 7)
+CPI_china_bp
+
+# Check for the statistical significance of the break-points
+coef(CPI_china_bp)
+
+# Check for the statistical significance of the break-points
+addi_CPI_BP <- efp(CPI_china ~ breakfactor(CPI_china_bp), type = "OLS-CUSUM")
+plot(addi_CPI_BP)
 
 ############################## End of Code #############################
