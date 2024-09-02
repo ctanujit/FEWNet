1279444e206c655502078d723491d9216eaaef2f
SHOVON SENGUPTA <ssg.plabon@gmail.com>
2024-09-02 10:58:35 -0400

    Update and rename WARNNX_daubechies_wavelet_exog_BRIC_epu_gprc_12M.R to FEWNet_daubechies_wavelet_exog_BRIC_epu_gprc_12M.R

--- code/FEWNet/FEWNet_daubechies_wavelet_exog_BRIC_epu_gprc_12M.R
+++ code/FEWNet/FEWNet_daubechies_wavelet_exog_BRIC_epu_gprc_12M.R
@@ -1,31 +1,27 @@
-### Explore ARNN model with exogenous factors #####
-# Read the base Table
-setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/brazil")
+### Explore FEWNet model with exogenous factors and daubechies wavelet transform #####
+##################### Brazil : 12M #################
+set.seed(20240101) # For reproducibility, we are using this seed value
+# Set the working directory
+setwd("/FEWNet/dataset/brazil")
 getwd()
 
 # Train Data
 cpi.train.df<-read.csv("df_train_cpi_bzl_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.train.df))
-# View(tail(cpi.train.df))
 str(cpi.train.df)
 
 # Test Data
 cpi.test.df<-read.csv("df_test_cpi_bzl_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.test.df))
-# View(tail(cpi.test.df))
 str(cpi.test.df)
 
-# Convert the Date
-
-# Use Series cpi, EPU and GPR for this exercise
+# Use Series cpi, EPU and GPRC for this exercise
 # install.packages(c("dLagM","tictoc","lmtest","tseries","forecast","pracma","egcm"))
 # install.packages(c("nardl","dynlm","zoo"))
 # install.packages("egcm")
 # install.packages("arfima")
-#fitting ARNN model
 # install.packages("nnet")
 # install.packages("forecast")
 # install.packages('wavelets')
+
 library(lubridate)
 library(dLagM)
 library(tictoc)
@@ -34,31 +30,8 @@ library(tseries)
 library(forecast)
 library(pracma)
 library(wavelets)
-# library(egcm)
-
-
-
-str(cpi.train.df)
-str(cpi.test.df)
-
-
-
-cpi.df.train.ts <- ts(cpi.train.df$CPI_inflation_rate)
-cpi.df.train.ts
-
-cpi.df.test.ts <- ts(cpi.test.df$CPI_inflation_rate)
-cpi.df.test.ts
-
-
 library(nnet)
-library(forecast)
-library(wavelets)
-
-# Link : Covid Paper: https://arxiv.org/pdf/2010.05079.pdf
-# Link: https://cran.r-project.org/web/packages/forecast/index.html
-# ARFIMA documentation: https://cran.r-project.org/web/packages/arfima/arfima.pdf
-#fitting ARFIMA model
-# ?arfima
+# library(egcm)
 
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
@@ -121,16 +94,11 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
     ts <- NULL
     ts <- WS[,WVLevel]
     WaveletNARFit <- forecast::nnetar(y=as.ts(ts), 
-                                      # xreg = xreg_tr[,1:6], 
                                       xreg = as.data.frame(xMat.train.new[,1:6]),
                                       p = MaxARParam, 
                                       repeats = 500)
     WaveletNARPredict <- WaveletNARFit$fitted
     WaveletNARForecast <- forecast::forecast(WaveletNARFit, 
-                                             # xreg = xreg_tr[,1:6],
-                                             # Should this be set against the test dataset
-                                             # xreg = xreg_tst[,1:6],
-                                             # xreg = xreg_tst[,1:6],
                                              xreg = as.data.frame(xMat.train.new[1:12,1:6]),
                                              h=NForecast)
     AllWaveletPrediction <- cbind(AllWaveletPrediction,WaveletNARPredict)
@@ -141,14 +109,6 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
   return(list(Finalforecast=Finalforecast,FinalPrediction=FinalPrediction))
 }
 
-# View(head(cpi.train.df))
-# View(head(cpi.test.df))
-str(cpi.train.df)
-str(cpi.test.df)
-
-# floor(log(length(con_tr)))
-# length(con_tr)
-# ts(con_tr)
 # Training and Test dataset
 con_tr = cpi.train.df$CPI_inflation_rate
 xreg_tr = cbind(
@@ -167,7 +127,6 @@ colnames(xreg_tr) <- c('epu_cycle_cf_l1',
                        'cpi_cycle_cf_l1',
                        'cpi_trend_hp_l1')
 xreg_tst = cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.test.df$epu_cycle_cf_l1,
   cpi.test.df$epu_trend_hp_l1,
   cpi.test.df$gprc_ind_cycle_cf_l1,
@@ -184,14 +143,9 @@ colnames(xreg_tst) <- c('epu_cycle_cf_l1',
 
 con_tst = cpi.test.df$CPI_inflation_rate
 
-# View(head(xreg_tr))
-# View(head(xreg_tst))
-# View(tail(xreg_tst))
-
 xreg_tr[1:6]
 xreg_tr
 ####################### Proposed WARNNX ##########################
-
 # install.packages("wavelets")
 library(wavelets)
 #New Method -- Final selected Model
@@ -208,8 +162,6 @@ smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
 mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
 fore_warnnx$`fit_warnnx$Finalforecast`
 con_tst
-
-
 # > forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
 # ME     RMSE     MAE      MPE     MAPE
 # Test set 1.720785 2.760182 2.35422 14.78136 28.29256
@@ -226,28 +178,18 @@ con_tst
 ########################## End of Code: Brazil ############################################
 
 ############################### Code for Russia ##################################
-### Explore ARNN model with exogenous factors #####
-# Read the base Table
-setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/russia")
+### Explore FEWNet model with exogenous factors #####
+# Set the working directory
+setwd("/FEWNet/dataset/russia")
 getwd()
 
 # Train Data
 cpi.train.df<-read.csv("df_train_cpi_rus_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.train.df))
-# View(tail(cpi.train.df))
 str(cpi.train.df)
 
 # Test Data
 cpi.test.df<-read.csv("df_test_cpi_rus_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.test.df))
-# View(tail(cpi.test.df))
 str(cpi.test.df)
-
-# Convert the Date
-library(lubridate)
-# cpi.train.df$date <- dmy(cpi.train.df$date)
-# head(cpi.train.df)
-# tail(cpi.train.df)
 
 # Use Series cpi, EPU and GPR for this exercise
 library(dLagM)
@@ -257,32 +199,9 @@ library(tseries)
 library(forecast)
 library(pracma)
 library(wavelets)
+library(nnet)
 # library(egcm)
 
-str(cpi.train.df)
-str(cpi.test.df)
-
-str(cpi.train.df)
-cpi.df.train.ts <- ts(cpi.train.df$cpi_inflation_rate)
-cpi.df.train.ts
-
-cpi.df.test.ts <- ts(cpi.test.df$cpi_inflation_rate)
-cpi.df.test.ts
-
-# install.packages("arfima")
-#fitting ARNN model
-
-# install.packages("nnet")
-# install.packages("forecast")
-
-library(nnet)
-library(forecast)
-
-# Link : Covid Paper: https://arxiv.org/pdf/2010.05079.pdf
-# Link: https://cran.r-project.org/web/packages/forecast/index.html
-# ARFIMA documentation: https://cran.r-project.org/web/packages/arfima/arfima.pdf
-#fitting ARFIMA model
-# ?arfima
 
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
@@ -344,16 +263,11 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
     ts <- NULL
     ts <- WS[,WVLevel]
     WaveletNARFit <- forecast::nnetar(y=as.ts(ts), 
-                                      # xreg = xreg_tr[,1:6], 
                                       xreg = as.data.frame(xMat.train.new[,1:6]),
                                       p = MaxARParam, 
                                       repeats = 500)
     WaveletNARPredict <- WaveletNARFit$fitted
     WaveletNARForecast <- forecast::forecast(WaveletNARFit, 
-                                             # xreg = xreg_tr[,1:6],
-                                             # Should this be set against the test dataset
-                                             # xreg = xreg_tst[,1:6],
-                                             # xreg = xreg_tst[,1:6],
                                              xreg = as.data.frame(xMat.train.new[1:12,1:6]),
                                              h=NForecast)
     AllWaveletPrediction <- cbind(AllWaveletPrediction,WaveletNARPredict)
@@ -407,10 +321,6 @@ colnames(xreg_tst) <- c('epu_cycle_cf_l1',
 
 con_tst = cpi.test.df$cpi_inflation_rate
 
-# View(head(xreg_tr))
-# View(head(xreg_tst))
-# View(tail(xreg_tst))
-
 xreg_tr[1:6]
 xreg_tr
 
@@ -446,25 +356,19 @@ con_tst
 ########################## End of Code: Russia ############################################
 
 ############################### Code for India ##################################
-### Explore ARNN model with exogenous factors #####
-# Read the base Table
-setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/india")
+### Explore FEWNet model with exogenous factors #####
+# Set the working directory
+setwd("/FEWNet/dataset/india")
 getwd()
 
 # Train Data
 cpi.train.df<-read.csv("df_train_cpi_ind_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.train.df))
-# View(tail(cpi.train.df))
 str(cpi.train.df)
 
 # Test Data
 cpi.test.df<-read.csv("df_test_cpi_ind_lag_all_12M_R.csv",header=TRUE)
-# View(head(cpi.test.df))
-# View(tail(cpi.test.df))
 str(cpi.test.df)
 
-# Convert the Date
-library(lubridate)
 # Use Series cpi, EPU and GPR for this exercise
 library(dLagM)
 library(tictoc)
@@ -473,34 +377,10 @@ library(tseries)
 library(forecast)
 library(pracma)
 library(wavelets)
-# library(egcm)
-
-str(cpi.train.df)
-cpi.df.train.ts <- ts(cpi.train.df$CPI_inflation_Rate)
-cpi.df.train.ts
-
-cpi.df.test.ts <- ts(cpi.test.df$CPI_inflation_Rate)
-cpi.df.test.ts
-
-# install.packages("arfima")
-#fitting ARNN model
-
-# install.packages("nnet")
-# install.packages("forecast")
-
 library(nnet)
-library(forecast)
-library(wavelets)
-
-# Link : Covid Paper: https://arxiv.org/pdf/2010.05079.pdf
-# Link: https://cran.r-project.org/web/packages/forecast/index.html
-# ARFIMA documentation: https://cran.r-project.org/web/packages/arfima/arfima.pdf
-#fitting ARFIMA model
-# ?arfima
 
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.train.df$epu_cycle_cf_l1,
   cpi.train.df$epu_trend_hp_l1,
   cpi.train.df$gprc_ind_cycle_cf_l1,
@@ -512,7 +392,6 @@ ncol=6)
 xMat.train.new
 
 xMat.test.new <- matrix(cbind(
-  # cpi.test.df$CPI_inflation_Rate_l1,
   cpi.test.df$epu_cycle_cf_l1,
   cpi.test.df$epu_trend_hp_l1,
   cpi.test.df$gprc_ind_cycle_cf_l1,
@@ -557,16 +436,11 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
     ts <- NULL
     ts <- WS[,WVLevel]
     WaveletNARFit <- forecast::nnetar(y=as.ts(ts), 
-                                      # xreg = xreg_tr[,1:6], 
                                       xreg = as.data.frame(xMat.train.new[,1:6]),
                                       p = MaxARParam, 
                                       repeats = 500)
     WaveletNARPredict <- WaveletNARFit$fitted
     WaveletNARForecast <- forecast::forecast(WaveletNARFit, 
-                                             # xreg = xreg_tr[,1:6],
-                                             # Should this be set against the test dataset
-                                             # xreg = xreg_tst[,1:6],
-                                             # xreg = xreg_tst[,1:6],
                                              xreg = as.data.frame(xMat.train.new[1:12,1:6]),
                                              h=NForecast)
     AllWaveletPrediction <- cbind(AllWaveletPrediction,WaveletNARPredict)
@@ -577,18 +451,10 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
   return(list(Finalforecast=Finalforecast,FinalPrediction=FinalPrediction))
 }
 
-# View(head(cpi.train.df))
-# View(head(cpi.test.df))
-str(cpi.train.df)
-str(cpi.test.df)
 
-# floor(log(length(con_tr)))
-# length(con_tr)
-# ts(con_tr)
 # Training and Test dataset
 con_tr = cpi.train.df$CPI_inflation_Rate
 xreg_tr = cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.train.df$epu_cycle_cf_l1,
   cpi.train.df$epu_trend_hp_l1,
   cpi.train.df$gprc_ind_cycle_cf_l1,
@@ -603,7 +469,6 @@ colnames(xreg_tr) <- c('epu_cycle_cf_l1',
                        'cpi_cycle_cf_l1',
                        'cpi_trend_hp_l1')
 xreg_tst = cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.test.df$epu_cycle_cf_l1,
   cpi.test.df$epu_trend_hp_l1,
   cpi.test.df$gprc_ind_cycle_cf_l1,
@@ -620,14 +485,9 @@ colnames(xreg_tst) <- c('epu_cycle_cf_l1',
 
 con_tst = cpi.test.df$CPI_inflation_Rate
 
-# View(head(xreg_tr))
-# View(head(xreg_tst))
-# View(tail(xreg_tst))
-
 xreg_tr[1:6]
 xreg_tr
 ####################### Proposed WARNNX ##########################
-
 #New Method - Final Model
 set.seed(45)
 fit_warnnx = WaveletFittingnar(ts(con_tr), 
@@ -641,6 +501,7 @@ forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
 smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
 mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
 
+# Forecasts:
 fore_warnnx$`fit_warnnx$Finalforecast` # MaxARParam = 18
 con_tst
 
@@ -654,16 +515,15 @@ con_tst
 # > 
 # > fore_warnnx$`fit_warnnx$Finalforecast` # MaxARParam = 18
 # [1] 3.832395 4.880044 4.791201 3.567695 3.787484 3.927653 3.224295 3.331116 4.168042 5.380731 5.573936 5.908445
+# Test data
 # > con_tst
 # [1] 3.686636 3.162966 4.494493 5.665658 5.139860 5.257646 5.577841 5.263880 4.811442 4.403048 4.518828 4.837364
-
 ########################## End of Code: India ############################################
 
 ############################### Code for China ##################################
-### Explore WARNN model with exogenous factors #####
-# Read the base Table
-# setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/")
-setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/china")
+### Explore FEWNet model with exogenous factors #####
+# Set the working directory
+setwd("/FEWNet/dataset/china")
 getwd()
 
 # Train Data
@@ -682,45 +542,7 @@ library(tseries)
 library(forecast)
 library(pracma)
 library(wavelets)
-
-str(cpi.train.df)
-str(cpi.test.df)
-
-# create a matrix of external regressors
-xMat.train <- data.matrix(cbind(cpi.train.df$cpi_inflation_rate,
-                                cpi.train.df$epu_cycle_cf_l1,
-                                cpi.train.df$epu_trend_hp_l1,
-                                cpi.train.df$gprc_chn_cycle_cf_l1,
-                                cpi.train.df$gprc_chn_trend_hp_l1,
-                                cpi.train.df$cpi_cycle_cf_l1,
-                                cpi.train.df$cpi_trend_hp_l1))
-xMat.train
-
-xMat.test <- data.matrix(cbind(cpi.test.df$cpi_inflation_rate,
-                               cpi.test.df$epu_cycle_cf_l1,
-                               cpi.test.df$epu_trend_hp_l1,
-                               cpi.test.df$gprc_chn_cycle_cf_l1,
-                               cpi.test.df$gprc_chn_trend_hp_l1,
-                               cpi.test.df$cpi_cycle_cf_l1,
-                               cpi.test.df$cpi_trend_hp_l1))
-xMat.test
-
-str(cpi.train.df)
-cpi.df.train.ts <- ts(cpi.train.df$cpi_inflation_rate)
-cpi.df.train.ts
-
-cpi.df.test.ts <- ts(cpi.test.df$cpi_inflation_rate)
-cpi.df.test.ts
-
 library(nnet)
-library(forecast)
-library(wavelets)
-
-# Link : Covid Paper: https://arxiv.org/pdf/2010.05079.pdf
-# Link: https://cran.r-project.org/web/packages/forecast/index.html
-# ARFIMA documentation: https://cran.r-project.org/web/packages/arfima/arfima.pdf
-#fitting ARFIMA model
-# ?arfima
 
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
@@ -749,13 +571,6 @@ xMat.test.new
 # print the first 6 rows of 6 columns of the matirix
 xMat.test.new[1:6,1:6]
 
-# Image result for Slicing matrix data structure in R
-# Slice a Matrix
-# matrix_c[1,2] selects the element at the first row and second column.
-# matrix_c[1:3,2:3] results in a R slice matrix with the data on the rows 1, 2, 3 and columns 2, 3,
-# matrix_c[,1] selects all elements of the first column.
-# matrix_c[1,] selects all elements of the first row.
-str(cpi.train.df)
 cpi.df.train.ts <- ts(cpi.train.df$cpi_inflation_rate)
 cpi.df.train.ts
 
@@ -789,16 +604,11 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
     ts <- NULL
     ts <- WS[,WVLevel]
     WaveletNARFit <- forecast::nnetar(y=as.ts(ts),
-                                      # xreg = xreg_tr[,1:6],
                                       xreg = as.data.frame(xMat.train.new[,1:6]),
                                       p = MaxARParam,
                                       repeats = 500)
     WaveletNARPredict <- WaveletNARFit$fitted
     WaveletNARForecast <- forecast::forecast(WaveletNARFit,
-                                             # xreg = xreg_tr[,1:6],
-                                             # Should this be set against the test dataset
-                                             # xreg = xreg_tst[,1:6],
-                                             # xreg = xreg_tst[,1:6],
                                              xreg = as.data.frame(xMat.train.new[1:12,1:6]),
                                              h=NForecast)
     AllWaveletPrediction <- cbind(AllWaveletPrediction,WaveletNARPredict)
@@ -809,18 +619,9 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
   return(list(Finalforecast=Finalforecast,FinalPrediction=FinalPrediction))
 }
 
-# View(head(cpi.train.df))
-# View(head(cpi.test.df))
-str(cpi.train.df)
-str(cpi.test.df)
-con_tr = cpi.train.df$cpi_inflation_rate
-floor(log(length(con_tr)))
-length(con_tr)
-ts(con_tr)
 # Training and Test dataset
 con_tr = cpi.train.df$cpi_inflation_rate
 xreg_tr = cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.train.df$epu_cycle_cf_l1,
   cpi.train.df$epu_trend_hp_l1,
   cpi.train.df$gprc_chn_cycle_cf_l1,
@@ -835,7 +636,6 @@ colnames(xreg_tr) <- c('epu_cycle_cf_l1',
                        'cpi_cycle_cf_l1',
                        'cpi_trend_hp_l1')
 xreg_tst = cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.test.df$epu_cycle_cf_l1,
   cpi.test.df$epu_trend_hp_l1,
   cpi.test.df$gprc_chn_cycle_cf_l1,
@@ -852,14 +652,9 @@ colnames(xreg_tst) <- c('epu_cycle_cf_l1',
 
 con_tst = cpi.test.df$cpi_inflation_rate
 
-# View(head(xreg_tr))
-# View(head(xreg_tst))
-# View(tail(xreg_tst))
-
 xreg_tr[1:6]
 xreg_tr
 ####################### Proposed WARNNX ##########################
-
 #New Method - Final Model
 set.seed(45)
 fit_warnnx = WaveletFittingnar(ts(con_tr),
