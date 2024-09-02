8c482ebb5470a94feebe3211ef0d4d189ed8c8c1
SHOVON SENGUPTA <ssg.plabon@gmail.com>
2024-09-02 13:08:34 -0400

    Update and rename WARNNX_exog_bzl_bl14_cpi_epu_gprc_cycle_trend_24M_V2.R to FEWNet_exog_bzl_bl14_cpi_epu_gprc_cycle_trend_24M_V2.R

---
+++ code/FEWNet/FEWNet_exog_bzl_bl14_cpi_epu_gprc_cycle_trend_24M_V2.R
@@ -0,0 +1,163 @@
+### Explore FEWNet model with exogenous factors #####
+# Set the working directory
+setwd("/FEWNet/dataset/brazil")
+getwd()
+
+# Train Data
+cpi.train.df<-read.csv("df_train_cpi_bzl_lag_all_24M_R.csv",header=TRUE)
+str(cpi.train.df)
+
+# Test Data
+cpi.test.df<-read.csv("df_test_cpi_bzl_lag_all_24M_R.csv",header=TRUE)
+str(cpi.test.df)
+
+
+library(dLagM)
+library(tictoc)
+library(lmtest)
+library(tseries)
+library(forecast)
+library(pracma)
+library(nnet)
+
+
+# create a matrix of external regressors
+xMat.train.new <- matrix(cbind(
+  # cpi.train.df$CPI_inflation_Rate_l1,
+  cpi.train.df$epu_cycle_cf_l1,
+  cpi.train.df$epu_trend_hp_l1,
+  cpi.train.df$gprc_ind_cycle_cf_l1,
+  cpi.train.df$gprc_ind_trend_hp_l1,
+  cpi.train.df$cpi_cycle_cf_l1,
+  cpi.train.df$cpi_trend_hp_l1
+),
+ncol=6)
+xMat.train.new
+
+xMat.test.new <- matrix(cbind(
+  # cpi.test.df$CPI_inflation_Rate_l1,
+  cpi.test.df$epu_cycle_cf_l1,
+  cpi.test.df$epu_trend_hp_l1,
+  cpi.test.df$gprc_ind_cycle_cf_l1,
+  cpi.test.df$gprc_ind_trend_hp_l1,
+  cpi.test.df$cpi_cycle_cf_l1,
+  cpi.test.df$cpi_trend_hp_l1
+),
+ncol=6)
+xMat.test.new
+xMat.test.new[1:6,1:6]
+
+cpi.df.train.ts <- ts(cpi.train.df$CPI_inflation_rate)
+cpi.df.train.ts
+
+cpi.df.test.ts <- ts(cpi.test.df$CPI_inflation_rate)
+cpi.df.test.ts
+
+##################### LIBRARY ###########################################################
+library(tseries)
+library(forecast)
+library(Metrics)
+library(nonlinearTseries)
+library(lmtest)
+
+################### Code for the WARNNX in R #######################
+WaveletFitting <- function(ts,Wvlevels,bndry,FFlag)
+{
+  mraout <- wavelets::modwt(ts, filter='bl14', n.levels=Wvlevels,boundary=bndry, fast=FFlag)
+  WaveletSeries <- cbind(do.call(cbind,mraout@W),mraout@V[[Wvlevels]])
+  return(list(WaveletSeries=WaveletSeries,WVSeries=mraout))
+}
+
+WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NForecast)
+  
+{
+  WS <- WaveletFitting(ts=ts,Wvlevels=Waveletlevels,bndry=boundary,FFlag=FastFlag)$WaveletSeries
+  AllWaveletForecast <- NULL;AllWaveletPrediction <- NULL
+  
+  for(WVLevel in 1:ncol(WS))
+  {
+    ts <- NULL
+    ts <- WS[,WVLevel]
+    WaveletNARFit <- forecast::nnetar(y=as.ts(ts),
+                                      xreg = as.data.frame(xMat.train.new[,1:6]),
+                                      p = MaxARParam,
+                                      repeats = 500)
+    WaveletNARPredict <- WaveletNARFit$fitted
+    WaveletNARForecast <- forecast::forecast(WaveletNARFit,
+                                             xreg = as.data.frame(xMat.train.new[1:24,1:6]),
+                                             h=NForecast)
+    AllWaveletPrediction <- cbind(AllWaveletPrediction,WaveletNARPredict)
+    AllWaveletForecast <- cbind(AllWaveletForecast,as.matrix(WaveletNARForecast$mean))
+  }
+  Finalforecast <- rowSums(AllWaveletForecast,na.rm = T)
+  FinalPrediction <- rowSums(AllWaveletPrediction,na.rm = T)
+  return(list(Finalforecast=Finalforecast,FinalPrediction=FinalPrediction))
+}
+
+# Training and Test dataset
+con_tr = cpi.train.df$CPI_inflation_rate
+xreg_tr = cbind(
+  cpi.train.df$epu_cycle_cf_l1,
+  cpi.train.df$epu_trend_hp_l1,
+  cpi.train.df$gprc_ind_cycle_cf_l1,
+  cpi.train.df$gprc_ind_trend_hp_l1,
+  cpi.train.df$cpi_cycle_cf_l1,
+  cpi.train.df$cpi_trend_hp_l1)
+# assigning new names to the columns of the data frame
+colnames(xreg_tr) <- c('epu_cycle_cf_l1',
+                       'epu_trend_hp_l1',
+                       'gprc_ind_cycle_cf_l1',
+                       'gprc_ind_trend_hp_l1',
+                       'cpi_cycle_cf_l1',
+                       'cpi_trend_hp_l1')
+xreg_tst = cbind(
+  cpi.test.df$epu_cycle_cf_l1,
+  cpi.test.df$epu_trend_hp_l1,
+  cpi.test.df$gprc_ind_cycle_cf_l1,
+  cpi.test.df$gprc_ind_trend_hp_l1,
+  cpi.test.df$cpi_cycle_cf_l1,
+  cpi.test.df$cpi_trend_hp_l1)
+# assigning new names to the columns of the data frame
+colnames(xreg_tst) <- c('epu_cycle_cf_l1',
+                        'epu_trend_hp_l1',
+                        'gprc_ind_cycle_cf_l1',
+                        'gprc_ind_trend_hp_l1',
+                        'cpi_cycle_cf_l1',
+                        'cpi_trend_hp_l1')
+
+con_tst = cpi.test.df$CPI_inflation_rate
+
+xreg_tr[1:6]
+xreg_tr
+
+####################### Proposed FEWNet ##########################
+# source("warnnx.R")
+set.seed(42)
+fit_warnnx = WaveletFittingnar(ts(con_tr),
+                               Waveletlevels = floor(log(length(con_tr))),
+                               boundary = "periodic",
+                               FastFlag = TRUE,
+                               MaxARParam = 18, #best=18
+                               NForecast = 24)
+fore_warnnx = as.data.frame(fit_warnnx$Finalforecast, h = 24)
+forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
+smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
+mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
+fore_warnnx$`fit_warnnx$Finalforecast`
+con_tst
+
+# > forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
+# ME     RMSE      MAE       MPE     MAPE
+# Test set 0.5901395 2.822059 2.284711 -16.96181 48.25514
+# > smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
+# [1] 42.54726
+# > mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
+# [1] 4.654747
+# > fore_warnnx$`fit_warnnx$Finalforecast`
+# [1] 5.417258 5.027756 4.625646 4.639536 4.575335 4.753262 4.883850 4.943836 4.836718 4.610028 4.439504 4.554256 4.905307
+# [14] 5.356243 5.362974 4.711728 4.484754 4.848138 5.193589 5.295612 5.439580 5.163085 4.849177 5.110540
+# > con_tst
+# [1]  4.306152  4.191771  4.005114  3.303158  2.399279  1.877727  2.132417  2.305625  2.438465  3.135329  3.918350
+# [12]  4.311223  4.517457  4.559198  5.195379  6.099479  6.759116  8.056819  8.347072  8.994823  9.679774 10.246209
+# [23] 10.672622 10.738501
+
