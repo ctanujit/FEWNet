7210e43b6732c53932b58a52965f6ff62417c28b
SHOVON SENGUPTA <ssg.plabon@gmail.com>
2024-09-02 21:51:57 -0400

    Update EWNet_China_24M.R

--- code/FEWNet/EWNet_China_24M.R
+++ code/FEWNet/EWNet_China_24M.R
@@ -4,16 +4,33 @@
 setwd("/FEWNet/dataset/china")
 getwd()
 
+####################### EWNet with only EPU and GPRC as exog ##########################
+setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/china")
+getwd()
+
 # Train Data
 cpi.df <- read.csv("China_CPI_inf_rate_Monthly_mulvariate_epu_gprc_202201.csv",header=TRUE)
 str(cpi.df)
-# lets remove the first 12 observations to make the distribution comparable
+
 cpi.train.df<-cpi.df[13:203,1:4]
 str(cpi.train.df)
 
 # Test Data
 cpi.test.df<-cpi.df[204:227,1:4]
 str(cpi.test.df)
+
+
+# Use Series cpi, EPU and GPR for this exercise
+library(dLagM)
+library(tictoc)
+library(lmtest)
+library(tseries)
+library(forecast)
+library(pracma)
+library(egcm)
+library(nnet)
+library(forecast)
+
 
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
@@ -46,7 +63,7 @@ library(Metrics)
 library(nonlinearTseries)
 library(lmtest)
 
-################### Code for the EWNet in R #######################
+################### Code for the WARNNX in R #######################
 WaveletFitting <- function(ts,Wvlevels,bndry,FFlag)
 {
   mraout <- wavelets::modwt(ts, filter='haar', n.levels=Wvlevels,boundary=bndry, fast=FFlag)
@@ -80,6 +97,7 @@ WaveletFittingnar<- function(ts,Waveletlevels,boundary,FastFlag,MaxARParam,NFore
   return(list(Finalforecast=Finalforecast,FinalPrediction=FinalPrediction))
 }
 
+
 # Training and Test dataset
 con_tr = cpi.train.df$cpi_inflation_rate
 xreg_tr = cbind(
@@ -101,10 +119,12 @@ con_tst = cpi.test.df$cpi_inflation_rate
 
 head(xreg_tr)
 head(xreg_tst)
+# View(tail(xreg_tst))
 
 xreg_tr[1:6]
 xreg_tr
-####################### Proposed EWNet ##########################
+####################### Proposed WARNNX ##########################
+
 # source("warnnx.R")
 set.seed(42)
 fit_warnnx = WaveletFittingnar(ts(con_tr),
@@ -120,12 +140,18 @@ mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
 fit_warnnx$Finalforecast
 con_tst
 
-
+# p = 6
+# > forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
+#               ME     RMSE     MAE       MPE     MAPE
+# Test set -3.292997 3.761563 3.33431 -125.2685 560.7262
+# > smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
+# [1] 106.3732
+# > mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
+# [1] 4.690374
 # > fit_warnnx$Finalforecast
-# [1] 4.598191 4.998653 5.106492 5.158560 5.005111 5.275120 5.405772 5.551540 5.832547 5.848286 5.775940
-# [12] 5.739612 5.552026 5.298053 5.280936 5.297991 5.245335 5.264415 4.930678 4.676213 4.615073 4.331261
-# [23] 4.203108 4.125040
+# [1] 4.598195 4.998666 5.106519 5.158600 5.005163 5.275178 5.405838 5.551617 5.832642 5.848407 5.776095 5.739807 5.552272
+# [14] 5.298359 5.281323 5.298488 5.245974 5.265232 4.931706 4.677489 4.616641 4.333165 4.205348 4.127598
 # > con_tst
-# [1]  4.4131455  5.4205607  5.1803885  4.2711235  3.2467532  2.4118738  2.5069638  1.7576318  2.3875115
-# [10]  1.7304189  0.5415162 -0.4496403  0.2697842 -0.2216312 -0.4227999  0.3219466  0.9226485  1.6443494
-# [19]  1.2401969  1.9125597  0.6415800  0.4613803  1.4328405  2.4772978
+# [1]  4.4131455  5.4205607  5.1803885  4.2711235  3.2467532  2.4118738  2.5069638  1.7576318  2.3875115  1.7304189
+# [11]  0.5415162 -0.4496403  0.2697842 -0.2216312 -0.4227999  0.3219466  0.9226485  1.6443494  1.2401969  1.9125597
+# [21]  0.6415800  0.4613803  1.4328405  2.4772978
