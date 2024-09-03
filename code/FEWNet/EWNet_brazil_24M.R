3775bdce73b6330fd80ade0fe49dbda79a9cbc4b
SHOVON SENGUPTA <ssg.plabon@gmail.com>
2024-09-02 21:11:38 -0400

    Update EWNet_brazil_24M.R

--- code/FEWNet/EWNet_brazil_24M.R
+++ code/FEWNet/EWNet_brazil_24M.R
@@ -1,7 +1,10 @@
 ####################### EWNet with only EPU and GPRC as exog ##########################
-### Explore FEWNet model with exogenous factors #####
+### Explore EWNet model with exogenous factors #####
 # Set the working directory
 setwd("/FEWNet/dataset/brazil")
+getwd()
+
+setwd("/Users/shovonsengupta/Desktop/All/Time_Series_Forecasting_Research/Inflation_Forecasting_BRIC_Paper/data/BRIC_CPI_INF_UNCERT_data/base_data/brazil")
 getwd()
 
 # Train Data
@@ -9,19 +12,13 @@ cpi.df <- read.csv("Brazil_CPI_inf_rate_Monthly_base_mulvar_cpi_epu_gprc_202201.
 str(cpi.df)
 # lets remove the first 12 observations to make the distribution comparable
 cpi.train.df<-cpi.df[13:203,1:4]
-# View(head(cpi.train.df))
-# View(tail(cpi.train.df))
 str(cpi.train.df)
 
 # Test Data
 cpi.test.df<-cpi.df[204:227,1:4]
-# View(head(cpi.test.df))
-# View(tail(cpi.test.df))
 str(cpi.test.df)
 
-# Convert the Date
-library(lubridate)
-# Use Series cpi, EPU and GPR for this exercise
+
 library(dLagM)
 library(tictoc)
 library(lmtest)
@@ -36,7 +33,6 @@ str(cpi.train.df)
 str(cpi.test.df)
 # create a matrix of external regressors
 xMat.train.new <- matrix(cbind(
-  # cpi.train.df$CPI_inflation_Rate_l1,
   cpi.train.df$log_epu,
   cpi.train.df$gprc_bra
 ),
@@ -44,7 +40,6 @@ ncol=2)
 xMat.train.new
 
 xMat.test.new <- matrix(cbind(
-  # cpi.test.df$CPI_inflation_Rate_l1,
   cpi.test.df$log_epu,
   cpi.test.df$gprc_bra
 ),
@@ -130,68 +125,37 @@ head(xreg_tst)
 xreg_tr[1:6]
 xreg_tr
 
-####################### Proposed EWNet ##########################
+####################### Proposed WARNNX ##########################
 # source("warnnx.R")
-set.seed(50)
+set.seed(42)
 fit_warnnx = WaveletFittingnar(ts(con_tr),
                                Waveletlevels = floor(log(length(con_tr))),
                                boundary = "periodic",
                                FastFlag = TRUE,
-                               MaxARParam = 1, 
+                               MaxARParam = 36,
                                NForecast = 24)
 fore_warnnx = as.data.frame(fit_warnnx$Finalforecast, h = 24)
 forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
 smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
 mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
 fit_warnnx$Finalforecast
-# Forecasts:
-# 3.35
-# 3.38
-# 3.43
-# 3.15
-# 3.32
-# 3.87
-# 4.30
-# 4.68
-# 5.10
-# 5.31
-# 5.72
-# 6.08
-# 6.33
-# 6.57
-# 7.00
-# 6.85
-# 6.14
-# 5.90
-# 5.89
-# 5.68
-# 5.27
-# 5.53
-# 5.50
-# 5.08
 con_tst
-# 4.31
-# 4.19
-# 4.01
-# 3.30
-# 2.40
-# 1.88
-# 2.13
-# 2.31
-# 2.44
-# 3.14
-# 3.92
-# 4.31
-# 4.52
-# 4.56
-# 5.20
-# 6.10
-# 6.76
-# 8.06
-# 8.35
-# 8.99
-# 9.68
-# 10.25
-# 10.67
-# 10.74
+
+# p=36
+# > forecast::accuracy(fore_warnnx$`fit_warnnx$Finalforecast`, con_tst)
+#             ME     RMSE      MAE       MPE    MAPE
+# Test set 0.3644345 2.653998 2.219053 -16.29048 45.9106
+# > smape(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)*100
+# [1] 40.9394
+# > mase(con_tst, fore_warnnx$`fit_warnnx$Finalforecast`)
+# [1] 4.520978
+# > fit_warnnx$Finalforecast
+# [1] 3.345976 3.383090 3.433757 3.152540 3.321302 3.872703 4.299716 4.683655 5.100893 5.307291 5.724420 6.084339 6.332038
+# [14] 6.568987 6.998048 6.851960 6.140252 5.897747 5.893079 5.676773 5.265691 5.534906 5.499439 5.076032
+# > con_tst
+# [1]  4.306152  4.191771  4.005114  3.303158  2.399279  1.877727  2.132417  2.305625  2.438465  3.135329  3.918350
+# [12]  4.311223  4.517457  4.559198  5.195379  6.099479  6.759116  8.056819  8.347072  8.994823  9.679774 10.246209
+# [23] 10.672622 10.738501
+
+###################### End of Code #################
 
